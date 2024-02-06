fccfebbc5aa9cc9b44916c998c6a83b720e9a8ea
Danilo Massoni <massonidanilo@gmail.com>
2024-02-05 21:10:09 -0300

    first commit

---
+++ backend-vende-para-mim/src/main/java/com/danilomassoni/backendvendeparamim/BackendVendeParaMimApplication.java
@@ -0,0 +1,13 @@
+package com.danilomassoni.backendvendeparamim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVendeParaMimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVendeParaMimApplication.class, args);
+	}
+
+}
---
+++ backend-vende-para-mim/src/test/java/com/danilomassoni/backendvendeparamim/BackendVendeParaMimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilomassoni.backendvendeparamim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVendeParaMimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
